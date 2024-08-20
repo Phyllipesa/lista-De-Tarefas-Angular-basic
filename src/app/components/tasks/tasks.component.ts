a5c855b75245a830a711f3cde671293a27540472
Phyllipe <phyllipe.sjc@hotmail.com>
2024-08-19 21:07:45 -0300

    Criando a Task

---
+++ src/app/components/tasks/tasks.component.ts
@@ -0,0 +1,43 @@
+import { Component, OnInit } from '@angular/core';
+import { TaskService } from '../../services/task.service';
+import { Tarefa } from '../../tarefa';
+import { CommonModule } from '@angular/common';
+import { TaskItemComponent } from "../task-item/task-item.component";
+import { AddTaksComponent } from "../add-taks/add-taks.component";
+
+@Component({
+  selector: 'app-tasks',
+  standalone: true,
+  imports: [CommonModule, TaskItemComponent, AddTaksComponent],
+  templateUrl: './tasks.component.html',
+  styleUrl: './tasks.component.css'
+})
+export class TasksComponent implements OnInit {
+
+  tarefas: Tarefa[] = [];
+
+  constructor(private taskService: TaskService) { }
+
+  ngOnInit(): void {
+    this.taskService.getTasks().subscribe((dado) => {
+      this.tarefas = dado;
+      console.log(dado);
+    })
+  }
+
+  deleteTask(tarefa: Tarefa) {
+    this.taskService.deleteTask(tarefa).subscribe(() => {
+      this.tarefas = this.tarefas.filter((t) => t.id != tarefa.id);
+    })
+  }
+
+  toggleConcluido(tarefa: Tarefa) {
+    tarefa.concluido = !tarefa.concluido;
+    this.taskService.updateTask(tarefa).subscribe();
+  }
+
+  AddTask(tarefa: Tarefa) {
+    this.taskService.addTask(tarefa).subscribe((tarefa) => this.tarefas.push(tarefa));
+  }
+
+}
